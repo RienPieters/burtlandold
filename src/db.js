1d9631f0079b57e498053d09ee763190424c10bd
unknown <pietersrien7@gmail.com>
2023-10-27 11:21:38 +0200

    Adding variable for GOOGLE_CREDS

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase.json")
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
