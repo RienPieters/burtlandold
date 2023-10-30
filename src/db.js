83ab1b726da911b6988fe53381a4dc8cf9b8dad9
unknown <pietersrien7@gmail.com>
2023-10-30 11:17:19 +0100

    Fixed credentials :mag:

--- src/db.js
+++ src/db.js
@@ -1,6 +1,5 @@
 const admin = require("firebase-admin");
-// const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
-const serviceAccount = require("../firebase.json");
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
